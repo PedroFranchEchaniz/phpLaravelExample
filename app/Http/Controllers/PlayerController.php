19c0748e34668fc4c23b75b10d177bb3fd551646
Usuario <pedrofe.av@gmail.com>
2024-03-22 13:02:09 +0100

    ejemplo

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\http\Controllers;
+
+use App\Models\Player;
+
+use Illuminate\Http\Request;
+
+class PlayerController extends Controller
+{
+    //recuperar players
+    public function index(){
+
+        $players = Player::all();
+        return response()->json(players);
+    }
+
+    //guardar player
+    public function store (Request $request){
+        $validateData = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string'
+        ]);
+
+        $player = Player::create($validateData);
+
+        return response()->json($player, 201);
+    }
+
+    //obtener un player
+    public function show(Player $player){
+        return response()->json($player);
+    }
+
+    //editar un player
+    public function update (Request $request, Player $player){
+        $validateData = $request ->validate([
+            'name' => 'required|string',
+            'email' => 'required|string'
+        ]);
+
+        $player -> update ($validateData);
+        return response()->json($player, 200);
+    }
+}
